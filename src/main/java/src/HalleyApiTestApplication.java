7a551a096d3091afd5a5501182d92180c8443e86
matheuscarv69 <matheus9126@gmail.com9>
2023-02-25 15:26:29 -0400

    initial commit

---
+++ src/main/java/src/HalleyApiTestApplication.java
@@ -0,0 +1,13 @@
+package src;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HalleyApiTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HalleyApiTestApplication.class, args);
+    }
+
+}
